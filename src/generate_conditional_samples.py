7a62e71b5b5d9ebe18f2a593071c6e3cc96bbe6c
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-10-30 15:56:34 +0100

    refactor sample generation code for use in notebooks

--- src/generate_conditional_samples.py
+++ src/generate_conditional_samples.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import fire
 import json
 import os
 import numpy as np
@@ -10,59 +9,98 @@ from contextlib import contextmanager
 import model, sample, encoder
 
 @contextmanager
-def setup_model(model_name='117M',
+def setup_model(
+    model_name='117M',
     seed=None,
-    nsamples=0,
+    nsamples=1,
     batch_size=1,
     length=None,
     temperature=1,
     top_k=0,
     top_p=0.0
 ):
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
     enc = encoder.get_encoder(model_name)
     hparams = model.default_hparams()
     with open(os.path.join('models', model_name, 'hparams.json')) as f:
         hparams.override_from_dict(json.load(f))
 
     if length is None:
-        length = hparams.n_ctx
+        length = hparams.n_ctx // 2
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
     with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
         tf.set_random_seed(seed)
-
         output = sample.sample_sequence(
             hparams=hparams, length=length,
-            start_token=enc.encoder['<|endoftext|>'],
+            context=context,
             batch_size=batch_size,
             temperature=temperature, top_k=top_k, top_p=top_p
-        )[:, 1:]
+        )
 
         saver = tf.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
         saver.restore(sess, ckpt)
-        yield(sess, enc, output, batch_size, nsamples) 
+        yield (context, sess, enc, output, batch_size, nsamples)
 
-def iterate_samples(sess, enc, output, batch_size, nsamples):
-        generated = 0
-        while nsamples == 0 or generated < nsamples:
-            out = sess.run(output)
-            for i in range(batch_size):
-                generated += batch_size
-                text = enc.decode(out[i])
-                yield(generated, text)
+def iterate_samples(raw_text, context, sess, enc, output, batch_size, nsamples):
+    context_tokens = enc.encode(raw_text)
+    generated = 0
+    for _ in range(nsamples // batch_size):
+        out = sess.run(output, feed_dict={
+            context: [context_tokens for _ in range(batch_size)]
+        })[:, len(context_tokens):]
+        for i in range(batch_size):
+            generated += 1
+            text = enc.decode(out[i])
+            yield(generated, text)
 
-def print_samples(*args, **kwargs):
-    """
-    Run the sample_model
+def print_samples_for_initialized_model(raw_text, *args):
+    for (generated, text) in iterate_samples(raw_text, *args):
+        print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+        print(text)
+    print("=" * 80)
+
+def sample_model(raw_text, *args, **kwargs):
+    """ conditional samples
     :model_name=117M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to
-     reproduce results
-    :nsamples=0 : Number of samples to return, if 0, continues to
-     generate samples indefinately.
-    :batch_size=1 : Number of batches (only affects speed/memory).
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
+     results
+    :nsamples=1 : Number of samples to return total
+    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
+    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
+     overriding top_k if set to a value > 0. A good setting is 0.9.
+    """
+    texts = []
+    with setup_model(*args, **kwargs) as args2:
+        for (generated, text) in iterate_samples(raw_text, *args2):
+            texts.append(text)
+    return texts
+
+
+def print_samples(raw_text, *args, **kwargs):
+    """Print out conditional samples
+    :model_name=117M : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
+     results
+    :nsamples=1 : Number of samples to return total
+    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
     :length=None : Number of tokens in generated text, if None (default), is
      determined by model hyperparameters
     :temperature=1 : Float value controlling randomness in boltzmann
@@ -77,40 +115,5 @@ def print_samples(*args, **kwargs):
      overriding top_k if set to a value > 0. A good setting is 0.9.
     """
     with setup_model(*args, **kwargs) as args2:
-        for(generated, text) in iterate_samples(*args2):
-            print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-            print(text)
-        print("=" * 80)
+        print_samples_for_initialized_model(raw_text, *args)
 
-
-
-def sample_model(args, kwwargs):
-    """
-    Run the sample_model
-    :model_name=117M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to
-     reproduce results
-    :nsamples=0 : Number of samples to return, if 0, continues to
-     generate samples indefinately.
-    :batch_size=1 : Number of batches (only affects speed/memory).
-    :length=None : Number of tokens in generated text, if None (default), is
-     determined by model hyperparameters
-    :temperature=1 : Float value controlling randomness in boltzmann
-     distribution. Lower temperature results in less random completions. As the
-     temperature approaches zero, the model will become deterministic and
-     repetitive. Higher temperature results in more random completions.
-    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
-     considered for each step (token), resulting in deterministic completions,
-     while 40 means 40 words are considered at each step. 0 (default) is a
-     special setting meaning no restrictions. 40 generally is a good value.
-    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
-     overriding top_k if set to a value > 0. A good setting is 0.9.
-    """
-    samples = []
-    with setup_model(*args, **kwargs) as args2:
-        for(generated, text) in iterate_samples(*args2):
-            samples.append(text)
-    return samples
-
-if __name__ == '__main__':
-    fire.Fire(print_samples)
--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python3
 
 import fire
-import json
-import os
-import numpy as np
-import tensorflow as tf
+from generate_conditional_samples import setup_model, print_samples_for_initialized_model 
 
-import model, sample, encoder
-
-def interact_model(
-    model_name='117M',
-    seed=None,
-    nsamples=1,
-    batch_size=1,
-    length=None,
-    temperature=1,
-    top_k=0,
-    top_p=0.0
-):
+def interact_model(*args, **kwargs):
     """
     Interactively run the model
     :model_name=117M : String, which model to use
@@ -38,52 +24,13 @@ def interact_model(
     :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
      overriding top_k if set to a value > 0. A good setting is 0.9.
     """
-    if batch_size is None:
-        batch_size = 1
-    assert nsamples % batch_size == 0
-
-    enc = encoder.get_encoder(model_name)
-    hparams = model.default_hparams()
-    with open(os.path.join('models', model_name, 'hparams.json')) as f:
-        hparams.override_from_dict(json.load(f))
-
-    if length is None:
-        length = hparams.n_ctx // 2
-    elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
-
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
-        output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            context=context,
-            batch_size=batch_size,
-            temperature=temperature, top_k=top_k, top_p=top_p
-        )
-
-        saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
-        saver.restore(sess, ckpt)
-
+    with setup_model(*args, **kwargs) as args2:
         while True:
             raw_text = input("Model prompt >>> ")
             while not raw_text:
                 print('Prompt should not be empty!')
                 raw_text = input("Model prompt >>> ")
-            context_tokens = enc.encode(raw_text)
-            generated = 0
-            for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
-                for i in range(batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
-            print("=" * 80)
+            print_samples_for_initialized_model(raw_text, *args2)
 
 if __name__ == '__main__':
     fire.Fire(interact_model)
